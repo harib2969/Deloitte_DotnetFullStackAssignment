cfe5d13c3587818e5831fbac3892e0db30c8f2e8
harib2969 <88202748+harib2969@users.noreply.github.com>
2024-09-04 11:41:54 +0530

    Add files via upload
    Program is question 1
    Programsolutions is question 2

---
+++ DAY 2/2. ProgramSolution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Product ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Product name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Unit Price: ");
+            int price = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Quantity : ");
+            int quantity = int.Parse(Console.ReadLine());
+
+
+            int tamount = price * quantity;
+            int damount;
+            if (quantity > 50)
+            {
+                damount = (30 * tamount) / 100;
+            }
+            else if (quantity > 30)
+            {
+                damount = (20 * tamount) / 100;
+            }
+            else if (quantity > 10)
+            {
+                damount = (10 * tamount) / 100;
+            }
+            else
+            {
+                damount = 0;
+            }
+            int famount=tamount-damount;
+            Console.WriteLine();
+            Console.WriteLine("----BILL DETAILS----");
+            Console.WriteLine("Total Amount : " + tamount);
+            Console.WriteLine("Discount Amount : " + damount);
+            Console.WriteLine("Final amount : " + famount);
+
+            Console.ReadLine();
+        }
+    }
+}
