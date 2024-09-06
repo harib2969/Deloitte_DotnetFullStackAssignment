6a68bf6bc4f5e97e1f14b986ca94953a71f95170
harib2969 <88202748+harib2969@users.noreply.github.com>
2024-09-06 16:43:36 +0530

    Add files via upload
    dAY 4 assignments

---
+++ DAY 4/2.ProgramSolution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("\n Product Id: {0} \n Product Name: {1} \n Unit Price: {2} \n Quantity: {3}, Category: {4} \n", ProductId, Name, UnitPrice, Quantity, Category);
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connStr = "Server=USHYDCVENKATAK1\\SQLSERVER2022; Database=Sept2024Db; Integrated Security=true";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    Console.WriteLine("Database connected Successfully");
+                    Console.WriteLine("Enter Product Details\n");
+                    Product product= new Product();
+                    
+                    Console.WriteLine("Enter Product ID:");
+                    product.ProductId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Product Name:");
+                    product.Name = Console.ReadLine();
+
+                    Console.WriteLine("Enter Unit Price:");
+                    product.UnitPrice = float.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Quantity:");
+                    product.Quantity = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Product Category:");
+                    product.Category = Console.ReadLine();
+
+                    string cmdText = string.Format("INSERT INTO PRODUCT VALUES({0},'{1}',{2},{3},'{4}');",product.ProductId,product.Name,product.UnitPrice,product.Quantity,product.Category);
+                    
+                    cmd.CommandText = cmdText;
+
+                    int n = cmd.ExecuteNonQuery();  // For DML:  Insert, update, delete
+
+                    Console.WriteLine("Data Inserted Succesfully.\nNo. of rows affected :" + n);
+                    conn.Close();
+
+            };
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ DAY 4/3.ProgramSolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connStr = "Server=USHYDCVENKATAK1\\SQLSERVER2022; Database=Sept2024Db; Integrated Security=true";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    Console.WriteLine("Database connected Successfully");
+
+                    string cmdText = "SELECT * FROM PRODUCT";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmdText, connStr);
+                    DataSet ds = new DataSet();
+
+                    adapter.Fill(ds);
+                    Console.WriteLine("Product details:");
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", row[0].ToString(), row[1].ToString(), row[2].ToString(), row[3].ToString(), row[4].ToString());
+                    }
+
+            };
+            Console.ReadLine();
+
+        }
+    }
+
+}
